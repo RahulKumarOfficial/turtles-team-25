7b85213dc42d161c6c0099664687cf5b4cbe7037
rahulkumar082 <rahulkr9622@gmail.com>
2017-08-20 11:04:12 +0530

    added backgound

--- extension.js
+++ extension.js
@@ -1,56 +1,114 @@
 /// get time and date
 
-//$(document).ready(function() {
- 
-$('#clock12').hide();
 
-function ampmTime(){
-  var today = new Date();
-  var h = today.getHours();
-  var m = today.getMinutes();
-  var s = today.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s);
-   
-  if(h < 12){
-        $('#clock12').text(h + ":" + m + ":" + s + " AM");
-        var t = setTimeout(ampmTime, 500);
-    }
-    else{
-        h = h-12;
-        $('#clock12').text(h + ":" + m + ":" + s + " PM");
-        var t = setTimeout(ampmTime, 500);
-    }
-  
+/*
+function toggleTime(){
+    alert('Hi');
 }
-ampmTime();
-//$('#clock12').hide();
+*/
+$(document).ready(function() {
+ var images = [ 'backgrounds/light/cloudy-day.png', 'backgrounds/light/concrete-texture.png','backgrounds/light/congruent_pentagon.png','backgrounds/light/ep_naturalwhite.png','backgrounds/light/grey_@2X.png','backgrounds/light/topography.png','backgrounds/light/vertical-waves.png' ];
+  $("#changeimg").on('click',function(){
+    var ind = Math.floor((Math.random() * 8) +1 );
+   // console.log(ind);
+    $('body').css('background-image','url('+images[ind]+')');
+  });
+		// var index = 0;
+		// function changeImage(){
+		// document.getElementById('content').innerHTML=images[index];
+		// var img = images[index] ; document.getElementById('content').innerhtml('img')
+		// index++;
+		// index = index % 4; // This is for if this is the last image then goto first image I have 4 images so I've given 4 change accordingly
+		// img.src = images[index];
+		// }
+
+
 function startTime() {
-  
     var today = new Date();
     var h = today.getHours();
     var m = today.getMinutes();
     var s = today.getSeconds();
     m = checkTime(m);
     s = checkTime(s);
-    
-    var twenty = $('#clock24').text(h + ":" + m + ":" + s)
-    var b = setInterval(startTime, 500);
-}
-startTime();
 
+    window['h'] = h;
+    if(h < 12){
+        $('#clock').text(h + ":" + m + ":" + s + " AM");
+        var t = setTimeout(startTime, 500);
+    }
+    else{
+        h = h-12;
+        $('#clock').text(h + ":" + m + ":" + s + " PM");
+        var t = setTimeout(startTime, 500);
+    }
+/*
+    if(h < 12){
+        $('#clock').text(h + ":" + m + ":" + s + " AM");
 
-$('#toggle-time').click(function(){
-  $("#clock24,#clock12").toggle();
+        var t = setTimeout(startTime, 500);
+    }
+    else{
+        h = h-12;
+        $('#clock').text(h + ":" + m + ":" + s + " PM");
+        var t = setTimeout(startTime, 500);
+    }
+    */
+/*
+$('#clock').text(h + ":" + m + ":" + s );
+
+var t = setTimeout(startTime, 500);
+*/
+
+/*
+
+$("#toggle-time").on("click", function toggleTime(){
+    $(this).addClass("clicked");
+    if($("#toggle-time").hasClass("clicked")){
+        $('#clock').text(h + ":" + m + ":" + s );
+        var t = setTimeout(startTime, 500);
+    }
+    else{
+        if ($("#toggle-time").hasClass("clicked")){
+        if(h < 12){
+        $('#clock').text(h + ":" + m + ":" + s + " AM");
+        var t = setTimeout(startTime, 500);
+        }
+        else{
+        h = h-12;
+        $('#clock').text(h + ":" + m + ":" + s + " PM");
+        var t = setTimeout(startTime, 500);
+        }
+    }
+
+    window['toggleTime'] = toggleTime;
+
+    }
 });
+*/
 
-
+   /*
+if ($("#toggle-time").hasClass("clicked")){
+        if(h < 12){
+        $('#clock').text(h + ":" + m + ":" + s + " AM");
+        var t = setTimeout(startTime, 500);
+        }
+        else{
+        h = h-12;
+        $('#clock').text(h + ":" + m + ":" + s + " PM");
+        var t = setTimeout(startTime, 500);
+        }
+}
+else{
+    $('#clock').text(h + ":" + m + ":" + s);
+}
+*/
+}
 function checkTime(i) {
-    if (i < 10) {i = "0" + i};  
+    if (i < 10) {i = "0" + i};
     return i;
 }
 
-
+startTime();
 
 
 
@@ -91,7 +149,7 @@ var date = d.getDate();
 document.getElementById('date').innerHTML = date;
 
 
-//});
+});
 ///
 
 //Quote code
@@ -99,9 +157,9 @@ document.getElementById('date').innerHTML = date;
 var url = "https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
 var getQuote = function(data) {
   $(".quote-text").text('" '+data.quoteText+' "');
-  
+
  var quot = 'https://twitter.com/intent/tweet?text=' + '"'+ data.quoteText+ '"' + ' - ' + data.quoteAuthor;
-  
+
   if (data.quoteAuthor === '') {
     data.quoteAuthor = 'Unknown';
   }
@@ -118,7 +176,7 @@ $(document).ready(function() {
 
 $("#refresh").click(function() {
   $.getJSON(url, getQuote, 'jsonp');
-}); 
+});
 
 
 $(".twitter-share").on("click", function() {
@@ -137,9 +195,9 @@ $(document).ready(function(){
   //Check if navigator enabled
   if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(function(position) {
-     var lat = "lat=" +position.coords.latitude; 
+     var lat = "lat=" +position.coords.latitude;
      var lon = "lon=" + position.coords.longitude;
-   
+
     getWeather(lat, lon);
   });
 }
@@ -151,63 +209,57 @@ $(document).ready(function(){
 
 function getWeather(lat,lon){
   var urlstr = api +lat + "&" + lon;
- 
+
   $.ajax({
     url: urlstr,
   success:function(result) {
- // Get city and country from API   
+ // Get city and country from API
  $(".city").text(result.name);
  $(".country").text(result.sys.country);
     // Get local temprature
     var round = result.main.temp;
     var temp = Math.round(round);
-    
-$(".temprature").text(temp );
 
-$(".degree").text(String.fromCharCode(176))
-    
-$(".unit").text("C");
-    
-$(".unit").on("click", function() {
-  window.change = function (){
-	var x = $('.unit').text();
-	if (x.indexOf('C') == -1) {
-		var a = $('.temprature').text();
-		$('.temprature').text(Math.round((a - 32) * 500  / 9) / 100);
-		$(".unit").text('C');
-	} else {
-		var b = $('.temprature').text() * 9 / 5 + 32;
-		$('.temprature').text(Math.round(b * 100) / 100);
-		$(".unit").text('F');
-	}
-   
-  }
-change();
-});
-    
-  
-        
-    
+
+
+
+    $(".temprature").text(temp +" "+ String.fromCharCode(176));
+    //Toggle Farenheit
+    $("#farenheit").on("click",
+ function toggleFaren(){
+     var far = temp * 9/5 +32;
+     $(".temprature").text(Math.round(far)+" "+String.fromCharCode(176));
+      window['toggleFaren'] = toggleFaren;
+ });
+  //Toggle Celcius
+$("#celcius").on("click", function toggleCelcius(){
+
+    $(".temprature").text(temp+" "+String.fromCharCode(176));
+  window['toggleCelcius'] = toggleCelcius;
+  });
+
+
+
     //Get weather icons
     var icons = new Skycons({"color": "161616"});
     var desc= result.weather[0].main.toLowerCase();
-    
+
     var d = result.weather[0].description.toLowerCase();
     //distinguish between day and night icons
-    
+
     var iurl = result.weather[0].icon;
-    
-    
-    
+
+
+
     // checks wether it's night or day by looking at the letter before .png (n or d) using RegEx match
     var match = /.png/.exec(iurl);
     if (match){
         var i = match.index;
         var s = iurl[i-1];
-     
+
     }
-    
-    
+
+
     if(s == 'd' && desc == "clear"){
        desc = "clear sky-d";
        }
@@ -220,10 +272,10 @@ change();
     else if(s == 'd' && d == "few clouds"){
       desc = "few clouds-n";
     }
-    
+
     //Switch icons according to weather
     var idesc = "";
-    
+
     switch (desc){
       case "clear sky-d":
          idesc = "clear_day"
@@ -261,24 +313,21 @@ change();
       default:
         idesc = "cloudy"
         break;
-        
+
                  }
-      
+
 var iname = idesc.toUpperCase();
-    
+
 var call = "Skycons." + iname;
 var x = eval(call);
 icons.set("icon", x);
-    
+
 icons.play();
-   
+
   }
-    
-  
+
+
   });
 
 
 };
-
-
-
